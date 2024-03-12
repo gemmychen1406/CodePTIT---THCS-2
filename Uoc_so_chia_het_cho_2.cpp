9aa124bfec6f7a303d05d0e9c5c44d8df0183cae
Gemmy Chen <154255693+gemmychen1406@users.noreply.github.com>
2024-03-12 22:18:30 +0700

    Update Uoc_so_chia_het_cho_2.cpp

--- Uoc_so_chia_het_cho_2.cpp
+++ Uoc_so_chia_het_cho_2.cpp
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
 
-int main () {
-	int n;
-	scanf("%d", &n);
-	while(n--){
-		int t, x = 0;
-		scanf("%d", &t);
-	    for (int i = 1; i <= t; i++){
-	    	if (t % i == 0) {
-	    		if (i % 2 == 0) x++;
+int main(){
+	int t; scanf("%d",&t);
+	while(t--){
+		long long n; scanf("%lld",&n);
+		long long cnt = 0;
+		for(int i = 1;i<=sqrt(n);i++){
+			if(n % i == 0){
+				if(i%2 == 0) cnt++;
+				if((n/i) != i && (n/i) % 2 == 0) cnt++;
 			}
 		}
-		printf("%d\n", x);
+		printf("%lld\n",cnt);
 	}
-    return 0;
+	return 0;
 }
